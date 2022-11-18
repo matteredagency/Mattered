e2f40cee90030918ff5c6665d248b7e97a2a505b
Grant <gmontgomery1994@gmail.com>
2022-11-18 15:08:07 -0800

    webpack setup.

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/main.ts",
+  mode: "production",
+  context: __dirname,
+  resolve: {
+    extensions: ["*", ".js", ".ts"],
+    modules: [__dirname, "src", "node_modules"],
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: [/node_modules/],
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              minimize: true,
+              modules: {
+                localIdentName:
+                  process.env.NODE_ENV === "development"
+                    ? "[name]__[local]--[hash:base64:5]"
+                    : "[hash:base64:5]",
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.ts$/,
+        exclude: [/node_modules/],
+        loader: "ts-loader",
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+};
