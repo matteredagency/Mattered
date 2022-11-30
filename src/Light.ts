ceba1a947d257e933f14588caed818a072f2c82b
Grant <gmontgomery1994@gmail.com>
2022-11-30 14:37:13 -0800

    light source added.

--- src/Light.ts
+++ src/Light.ts
@@ -1,3 +1,22 @@
+import * as THREE from "three";
+import MatteredExperience from "./MatteredExperience";
+
 export default class Light {
-  constructor() {}
+  experience?: MatteredExperience;
+  directionalLight: THREE.DirectionalLight;
+  constructor() {
+    this.directionalLight = new THREE.DirectionalLight();
+    this.init();
+    this.experience = new MatteredExperience();
+    this.experience.scene?.add(this.directionalLight);
+    return this;
+  }
+
+  private init() {
+    this.directionalLight.intensity = 2;
+    this.directionalLight.position.set(0, 5, 5);
+    this.directionalLight.castShadow = true;
+    this.directionalLight.shadow.mapSize.width = 1024;
+    this.directionalLight.shadow.mapSize.height = 1024;
+  }
 }
