176e95e0370aa3771a5c7a3e2124306592daf0dd
Grant <gmontgomery1994@gmail.com>
2022-12-04 12:16:27 -0800

    changed webpack config to not output files in development.

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
   },
   output: {
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname, isDevelopment ? "public" : "dist"),
+    path: path.resolve(__dirname, !isDevelopment ? "dist" : "/public"),
   },
   optimization: {
     minimizer: [new CssMinimizerPlugin(), new UglifyJsPlugin()],
