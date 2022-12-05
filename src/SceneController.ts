9b23cd499e856ca05dd35c480fd8eb0f84290d69
Grant <gmontgomery1994@gmail.com>
2022-12-05 15:36:02 -0800

    scene controller can now use scroll percents accurately.

--- src/SceneController.ts
+++ src/SceneController.ts
@@ -7,13 +7,11 @@ export default class SceneController {
   }
 
   sceneSelct(currentPercent: number, isForward: boolean) {
-    switch (true) {
-      case currentPercent >= 10 && currentPercent <= 20:
-        console.log("first scene");
-      case currentPercent >= 20 && currentPercent <= 30:
-        console.log("second scene");
-      default:
-        return;
+    if (currentPercent >= 10 && currentPercent < 20) {
+      console.log("first function");
+    }
+    if (currentPercent >= 20 && currentPercent < 30) {
+      console.log("second function");
     }
   }
 }
