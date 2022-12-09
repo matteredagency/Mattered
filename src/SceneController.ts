b38e229080c2072255033d4d8d1d0fa528185bc2
Grant <gmontgomery1994@gmail.com>
2022-12-09 12:12:15 -0800

    rendering planets at certain points in the path.

--- src/SceneController.ts
+++ src/SceneController.ts
@@ -7,6 +7,9 @@ export default class SceneController {
   experience: MatteredExperience;
   planets: {
     venus: Planet;
+    earth: Planet;
+    mars: Planet;
+    jupiter: Planet;
   };
   constructor() {
     this.planets = {
@@ -14,7 +17,28 @@ export default class SceneController {
         createAssetPath("/objects/Venus.glb"),
         true,
         0.0005,
-        new THREE.Vector3(-130, 0, -330),
+        new THREE.Vector3(-550, 0, 200),
+        0.5
+      ),
+      earth: new Planet(
+        createAssetPath("/objects/Earth.glb"),
+        true,
+        0.0005,
+        new THREE.Vector3(525, 0, 0),
+        0.5
+      ),
+      mars: new Planet(
+        createAssetPath("/objects/Mars.glb"),
+        true,
+        0.0005,
+        new THREE.Vector3(-500, 0, -850),
+        0.5
+      ),
+      jupiter: new Planet(
+        createAssetPath("/objects/Jupiter.glb"),
+        true,
+        0.0005,
+        new THREE.Vector3(210, 0, -1050),
         0.5
       ),
     };
@@ -23,8 +47,29 @@ export default class SceneController {
   }
 
   sceneSelect(currentPercent: number) {
-    if (currentPercent >= 0 && currentPercent < 0.5) {
+    if (currentPercent >= 0 && currentPercent < 0.4) {
       this.planets.venus.init();
+    }
+    if (currentPercent >= 0.03 && currentPercent <= 0.25) {
+      this.planets.earth.init();
+    }
+    if (currentPercent > 0.22 && currentPercent < 0.52) {
+      this.planets.mars.init();
+    }
+    if (currentPercent > 0.23) {
+      this.planets.earth.remove();
+    }
+    if (currentPercent > 0.4) {
+      this.planets.venus.remove();
+    }
+    if (currentPercent > 0.52) {
+      this.planets.mars.remove();
+    }
+    if (currentPercent > 0.48 && currentPercent < 0.9) {
+      this.planets.jupiter.init();
+    }
+    if (currentPercent < 0.48) {
+      this.planets.jupiter.remove();
     }
   }
 }
