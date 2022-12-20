d93746c7f29be20c389d6a770c732988debc6f61
Grant <gmontgomery1994@gmail.com>
2022-12-20 14:10:43 -0800

    removed height and width requirement on svg.

--- src/intro/intro.ts
+++ src/intro/intro.ts
@@ -3,7 +3,5 @@ import "../../public/index.css";
 const triangle = document.querySelector(".cls-2");
 
 if (triangle) {
-  triangle.addEventListener("click", () => {
-    console.log("triangle clicked");
-  });
+  triangle.addEventListener("click", () => {});
 }
