c6c1a43c4f7bbc9951b911dfac3f3a2805f2e360
Grant <gmontgomery1994@gmail.com>
2022-12-21 10:39:05 -0800

    removed clearing timeout and autoplay on audio.

--- src/main/classes/Controls.ts
+++ src/main/classes/Controls.ts
@@ -12,17 +12,12 @@ export default class Controls {
     window.scroll({ behavior: "smooth" });
     this.oldScrollTop = 0;
 
-    let isScrolling: NodeJS.Timeout;
     this.scrollContainer?.addEventListener("scroll", (event) => {
       event.preventDefault();
-      window.clearTimeout(isScrolling);
       if (this.scrollContainer) {
-        this.experience.planeController.floatPlane = false;
         const { scrollHeight, scrollTop } = this.scrollContainer;
         this.scrollPercent = scrollTop / scrollHeight;
-        isScrolling = setTimeout(() => {
-          this.experience.planeController.floatPlane = true;
-        }, 150);
+
         this.experience.sceneController.sceneSelect(this.scrollPercent);
         this.experience.planeController.updatePlaneRotation(this.scrollPercent);
         this.experience.track.updateCameraPosition(this.scrollPercent);
