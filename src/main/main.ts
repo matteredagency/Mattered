ee6c317b03459222dda68ca6e045408dbb61d1cc
Grant <gmontgomery1994@gmail.com>
2022-12-30 12:05:21 -0800

    added grid helper back in scene.

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,8 +1,15 @@
+import THREE = require("three");
 import MatteredExperience from "./classes/MatteredExperience";
 import SoundController from "./SoundController";
 
-new MatteredExperience(
+const experience = new MatteredExperience(
   document.getElementById("canvas-scene") as HTMLCanvasElement
 );
+
+const size = 1000;
+const divisions = 10;
+
+const gridHelper = new THREE.GridHelper(size, divisions);
+experience.scene.add(gridHelper);
 
 new SoundController().animate();
