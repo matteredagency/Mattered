ca486bab6d05593e7294848fe7fec8f6455c868d
Grant <gmontgomery1994@gmail.com>
2023-01-17 15:56:11 -0800

    removed experience and grid helper.

--- src/main/main.ts
+++ src/main/main.ts
@@ -2,14 +2,9 @@ import MatteredExperience from "./classes/MatteredExperience";
 import SoundController from "./SoundController";
 import ChatBox from "./classes/ChatBox";
 
-import THREE from "./globalmports";
-
-const experience = new MatteredExperience(
+new MatteredExperience(
   document.getElementById("canvas-scene") as HTMLCanvasElement
 );
 
-const gridHelper = new THREE.GridHelper(1000, 10);
-
-experience.scene.add(gridHelper);
 // new ChatBox().init();
 new SoundController().animate();
