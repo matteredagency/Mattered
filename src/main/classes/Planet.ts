e6879b58ce13d7e5408bae6d27f2e19b6e90334d
Grant <gmontgomery1994@gmail.com>
2023-02-14 09:47:04 -0800

    Saturn reset.

--- src/main/classes/Planet.ts
+++ src/main/classes/Planet.ts
@@ -113,6 +113,14 @@ export default class Planet {
     );
     this.asset.add(this.experience.assets.assetsDirectory.objects[name]);
 
+    if (this.name === "Saturn") {
+      this.experience.assets.assetsDirectory.objects["Saturn"].scale.set(
+        1,
+        0.1,
+        1
+      );
+    }
+
     if (this.atmosphereRadius && this.atmosphereColor) this.addAtmosphere();
   }
 
